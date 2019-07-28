3d062da8acbc5abb10340c3ee065405ad205bedd
zhoucici <zch_gddr@163.com>
2019-07-28 16:21:00 +0800

    修改了一下路由文件路径，加了一个路由钩子

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import routeConfig from './routeConfig/index.js'
+
+Vue.use(Router)
+
+let routeMain = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: routeConfig
+  })
+
+export default routeMain
+
+
+
+routeMain.beforeEach((to, from, next) => {
+    let codelist = api 
+    if(to.meta.code==='1001'){
+      console.log('你的权限不足');
+      next({name:'about'})
+    }else{
+      next()
+    }
+    
+    })
+    
