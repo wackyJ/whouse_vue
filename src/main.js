c1ddc97af74516297a0ae8fb44ee1e69c2db4b3d
wackyJ <jdd@email.com>
2019-08-22 20:56:59 +0800

    修复部分文件错误

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 //为更加方便文件管理，我们把 router和store  独立到他们自己的一个文件夹下面
 import router from './router'
 
-import store from './vuex/index.js'
+import store from './store'
 
 Vue.config.productionTip = false
 
