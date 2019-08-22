70ae0377a159c2619ea67d8aff359cdeb9ab2edb
MissZither <1415179587@qq.com>
2019-08-22 20:36:44 +0800

    将文件归类，更新了主页的部分信息展示

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 //为更加方便文件管理，我们把 router和store  独立到他们自己的一个文件夹下面
-import router from './router/index.js'
+import router from './router'
 
 import store from './vuex/index.js'
 
 Vue.config.productionTip = false
-
 
 new Vue({
   router,
