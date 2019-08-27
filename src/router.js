96832970040e80bc03d9a4cfa4e4cc7b110905f6
wackyJ <jdd@email.com>
2019-08-27 19:23:13 +0800

    增加侧面栏对应页面组件

--- src/router.js
+++ src/router.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from './views/Index'
 import Repertory from './views/Repertory'
+import OrderPage from './views/OrderPage'
+import SetPage from './views/SetPage'
+import ProductPage from './views/ProductPage'
 import NotFound from './views/NotFound'
 
 Vue.use(Router)
@@ -13,7 +16,9 @@ export default new Router({
     {path:"/",component:Index},
     {path:"/index",component:Index},
     {path:"/repertory",component:Repertory},
+    {path:"/orderpage",component:OrderPage},
+    {path:"/setpage",component:SetPage},
+    {path:"/productpage",component:ProductPage},
     {path:"/*",component:NotFound}
-
   ]
 })
---
+++ src/views/ProductPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <main-aside></main-aside>
+    <main-header></main-header>
+    <div class="section">
+      <h2>商品页</h2>
+      <el-tabs type="border-card">
+        <el-tab-pane label="商品查询">商品查询</el-tab-pane>
+        <el-tab-pane label="商品状态">商品状态</el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .section{
+    width:96%;
+    height:100%;
+    min-width:1300px;
+    position:relative;
+    left:4%;
+    top:48px; 
+    /* padding:28px 65px 30px 65px; */
+    box-sizing: border-box;
+    background-color:#eceff3;
+  }
+</style>
