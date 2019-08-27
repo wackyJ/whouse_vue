3b5be6f33332e4e1a411f47db140af2688e9cfe4
luguohang <1584442062@qq.com>
2019-08-27 21:23:28 +0800

    上传轮播图

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="carousel">
-    轮播图
+     <el-carousel :interval="5000">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <img src="img/rc_01.png" alt="">
+    </el-carousel-item>
+  </el-carousel>
   </div>
 </template>
 
@@ -16,4 +20,20 @@ export default {
     background-color:#fff;
     height:300px;
   }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  
 </style>
