b68814fa29e8a6102ff2d038ed849290541a0f46
wackyJ <jdd@email.com>
2019-08-30 14:24:32 +0800

    修改订单页表单样式

--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -5,35 +5,37 @@
     <div class="order-page">
       <el-tabs type="border-card">
         <el-tab-pane label="订单提交">
-          <el-form ref="form" :model="sizeForm" label-width="100px" size="small " > 
+          <el-form ref="form" :model="orderForm" label-width="100px" size="small " :hide-required-asterisk=true> 
             <div class="merge">
-              <el-form-item label="订单编号">
-                <el-input v-model="sizeForm.onum"></el-input>
-              </el-form-item>
-              <el-form-item label="商品编号">
-                <el-input v-model="sizeForm.pid"></el-input>
+              <el-form-item label="订单编号" :required=true>
+                <el-input v-model="orderForm.onum" :required=true></el-input>
               </el-form-item>
             </div>
             <div class="merge">
-              <el-form-item label="商品单价">
-                <el-input v-model="sizeForm.sell_price"></el-input>
+              <el-form-item label="商品编号"> 
+                <el-input v-model="orderForm.orderProdcut[0].pid"></el-input>
               </el-form-item>
-              <el-form-item label="商品数量">
-                <el-input v-model="sizeForm.ocount"></el-input>
+              <el-form-item label="商品单价">
+                <el-input v-model="orderForm.orderProduct[0].sellprice"></el-input>
+              </el-form-item>
+              <!--<el-form-item label="商品数量">
+                <el-input v-model="orderForm.orderProduct[i].ocount"></el-input>
               </el-form-item>
               <el-form-item label="商品总价">
-                <el-input v-model="sizeForm.total" :disabled=true></el-input>
-              </el-form-item>
+                <el-input v-model="orderForm.orderProduct[i].total" :disabled=true></el-input>
+              </el-form-item>-->
+              <i title="单击按钮增加商品" class="el-icon-circle-plus-outline"
+              @click="addOrderProduct"></i>
             </div>
             <div class="merge">
               <el-form-item label="客户编号">
-                <el-input v-model="sizeForm.cid"></el-input>
+                <el-input v-model="orderForm.cid"></el-input>
               </el-form-item>
               <el-form-item label="客户备注">
-                <el-input v-model="sizeForm.remark"></el-input>
+                <el-input v-model="orderForm.remark"></el-input>
               </el-form-item>
               <el-form-item label="操作人员编号">
-                <el-input v-model="sizeForm.uid"></el-input>
+                <el-input v-model="orderForm.uid"></el-input>
               </el-form-item>
             </div>
             <el-form-item label="收货地址">
@@ -43,36 +45,36 @@
                   :options="options"
                   filterable
                   @change="getAdress"></el-cascader>
-                  <el-input v-model="sizeForm.lastAdress" placeholder="详细地址" style="width:52%;"></el-input>
+                  <el-input v-model="orderForm.lastAdress" placeholder="详细地址" style="width:100%;"></el-input>
               </div>
             </el-form-item>
             <el-form-item label="创建时间">
-              <el-col :span="8">
-                <el-date-picker type="date" placeholder="选择日期" v-model="sizeForm.create_date" style="width: 100%;"></el-date-picker>
+              <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期" v-model="orderForm.create_date" style="width: 100%;"></el-date-picker>
               </el-col>
-              <el-col class="line" :span="1">-</el-col>
-              <el-col :span="8">
-                <el-time-picker placeholder="选择时间" v-model="sizeForm.create_date" style="width: 100%;"></el-time-picker>  
+              <el-col class="line" :span="2">-</el-col>
+              <el-col :span="11">
+                <el-time-picker placeholder="选择时间" v-model="orderForm.create_date" style="width: 100%;"></el-time-picker>  
               </el-col>
             </el-form-item>
              <el-form-item label="发货时间">
-              <el-col :span="8">
-                <el-date-picker type="date" placeholder="选择日期" v-model="sizeForm.delivery_date" style="width: 100%;"></el-date-picker>
+              <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期" v-model="orderForm.delivery_date" style="width: 100%;"></el-date-picker>
               </el-col>
-              <el-col class="line" :span="1">-</el-col>
-              <el-col :span="8">
-                <el-time-picker placeholder="选择时间" v-model="sizeForm.delivery_date" style="width: 100%;"></el-time-picker>
+              <el-col class="line" :span="2">-</el-col>
+              <el-col :span="11">
+                <el-time-picker placeholder="选择时间" v-model="orderForm.delivery_date" style="width: 100%;"></el-time-picker>
               </el-col>
             </el-form-item>
             <!-- <el-form-item label="活动性质">
-              <el-checkbox-group v-model="sizeForm.type">
+              <el-checkbox-group v-model="orderForm.type">
                 <el-checkbox-button label="美食/餐厅线上活动" name="type"></el-checkbox-button>
                 <el-checkbox-button label="地推活动" name="type"></el-checkbox-button>
                 <el-checkbox-button label="线下主题活动" name="type"></el-checkbox-button>
               </el-checkbox-group>
             </el-form-item> -->
             <el-form-item label="订单状态">
-              <el-radio-group v-model="sizeForm.ostatus" size="mini">
+              <el-radio-group v-model="orderForm.ostatus" size="mini">
                 <el-radio border label="待付款"></el-radio>
                 <el-radio border label="待审核"></el-radio>
                 <el-radio border label="异常"></el-radio>
@@ -91,7 +93,7 @@
         </el-tab-pane>
         <el-tab-pane label="订单查询">订单查询</el-tab-pane>
         <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-        <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        <el-tab-pane label="定时任务补偿">{{orderForm.orderProdcut[0].pid}}</el-tab-pane>
       </el-tabs>
     </div>
   </div>
@@ -102,12 +104,15 @@
   export default {
     data() {
       return {
-        sizeForm: {
+        i:0,
+        orderForm: {
           onum: '',
-          pid: '',
-          sell_price: 0,
-          ocount: 0,
-          total:0,
+          orderProdcut:[{
+            pid: 22222,
+            sellprice: 0,
+            ocount: 0,
+            total:0
+          }],
           cid: '',
           create_date: '',
           delivery_date: '',
@@ -120,24 +125,33 @@
       };
     },
     methods: {
+      /*addOrderProduct(e){
+        var goods=e.target.parentNode;
+        // var newP=
+      }
       onSubmit() {
-      //  console.log('submit!');
+        /*this.axios.post("/order/v1/createOrder",{
+          params:{
+            orderForm:this.orderForm
+          }}).then(result=>{
+
+        })
       },
       getAdress(val){
-        this.sizeForm.firstAdress=val;
+        this.orderForm.firstAdress=val;
       }
-    },
-    computed:{
+      }*/
+    /*computed:{
       newTotal(){
-        return Number(this.sizeForm.sell_price)*Number(this.sizeForm.ocount);
+        return Number(this.orderForm.orderProdcut[i].sell_price)*Number(this.orderForm.ocount);
       }
     },
     watch:{
       newTotal(val){
-        this.sizeForm.total=val;
+        this.orderForm.total=val;
       }
-    }
-  }
+    }*/
+  }}
 </script>
 
 <style scoped>
@@ -146,20 +160,32 @@
     min-width:1300px;
     position:absolute;
     top:48px;
-    padding:28px 65px 30px 65px;
+    padding-left:2%;
     box-sizing: border-box;
     left: 2%;
   }
-  .merge{
-    display: flex;
-  }
-  /*input.el-input__inner{
-    border:1px solid red;
-  }
   .el-tabs{
-    width: 1%;
+    box-shadow: none;
+    border:none;
   }
-  .el-radio-group{
-    width:50%;
-  }*/
+  .el-tab-pane{
+    /* border:1px solid black; */
+    width: 70%;
+    margin: 2% auto;
+  }
+  .merge>div.el-form-item{
+    /* border:1px solid red; */
+    padding-right:0;
+    margin-bottom:0;
+  }
+  .merge,.el-radio-group{
+    /* border: 2px solid black; */
+    display: flex;
+    align-items: center;
+    margin-bottom: 18px;
+    justify-content: space-between;
+  }
+  .el-radio{
+    margin:0;
+  }
 </style>
