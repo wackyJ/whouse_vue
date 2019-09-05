c57b5da7ef67abaa2815190c1f1aa3a7c95f838c
wackyJ <jdd@email.com>
2019-09-05 13:13:40 +0800

    增加判断用户是否保持活跃状态，无任何操作30分钟后自动跳转回登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,45 @@
 <template>
-  <div id="app">
+  <div id="app"  @mousemove="moveEvent" @click="moveEvent">
     <router-view/>
   </div>
 </template>
+
+<script>
+  
+export default {
+  name: 'App',
+  /*** 监听浏览器刷***/
+  data(){
+    return {
+      timmer: null,
+    }
+  },
+  methods: {
+    moveEvent(){
+//		console.log("清除了定时器！！！！！")
+      let path = ['/login']
+      if(!path.includes(this.$route.path)) { //如果不是登录页面的话页面停止进行30分钟后清空session
+        clearTimeout(this.timmer);
+        this.init();
+      }
+    },
+    init(){
+      this.timmer=setTimeout(()=>{
+        //清除session
+        sessionStorage.removeItem("sessionData");
+        sessionStorage.clear();
+        //清除缓存
+        // this.$cache.reset();
+        console.log(this.$cache);
+        //跳往登录页面
+        this.$router.push({
+          path: "/login",
+      });
+      },30*60*1000);//设置半小时清空session进入屏保
+    },
+  }
+}
+</script>
 
 <style lang="less">
 @import url("./assets/css/base.css");
