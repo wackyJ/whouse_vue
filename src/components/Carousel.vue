789604b6e75b3d79b7fe16d5d48e1c0627655d7b
MissZither <1415179587@qq.com>
2019-09-07 11:38:42 +0800

    修改了轮播图的样式

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="carousel">
-     <el-carousel :interval="5000">
-    <el-carousel-item v-for="item in 4" :key="item">
-      <img src="img/rc_01.png" alt="">
-    </el-carousel-item>
-  </el-carousel>
+    <el-carousel :interval="5000">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <img src="img/rc_01.png">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
@@ -20,18 +20,17 @@ export default {
     background-color:#fff;
     height:310px;
   }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
+  .el-carousel,.el-carousel__container {
+    width:100%;
+    height:310px;
   }
-  
+  .el-carousel__item img {
+    width:100%;
+    height:100%;
+  }
   .el-carousel__item:nth-child(2n) {
     background-color: #99a9bf;
   }
-  
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
   }
