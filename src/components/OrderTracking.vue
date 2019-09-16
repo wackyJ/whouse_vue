132d657166afa949f9940e8bff0862f2df71ddaf
wackyJ <785273094@qq.com>
2019-09-16 16:57:56 +0800

    增加快递即时查询子组件（测试版）

---
+++ src/components/OrderTracking.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    商户ID：<input type="text" v-model="ShipperCode"><br>
+    订单ID：<input type="text" v-model="LogisticCode"><br>
+    <button @click="tracking">search</button>
+    <div>{{goodsinfo}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      goodsinfo:'',
+      ShipperCode:'',
+      LogisticCode:''
+    }
+  },
+  methods:{
+    tracking(){
+      this.axios.get("/logistics/v1/immediateQuery",{
+        params:{
+          ShipperCode:this.ShipperCode,
+          LogisticCode:this.LogisticCode
+        }
+      }).then(result=>{
+        this.goodsinfo=result.data;
+      })
+    }
+  }
+}
+</script>
